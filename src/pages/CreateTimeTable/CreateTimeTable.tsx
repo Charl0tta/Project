e93d69d7abf57d0c20af9562a9d529c85cfc52a7
Charl0tta <eugene.ya@mail.ru>
2024-02-25 16:30:51 +0300

    Ver 3

--- src/pages/CreateTimeTable/CreateTimeTable.tsx
+++ src/pages/CreateTimeTable/CreateTimeTable.tsx
@@ -2,23 +2,22 @@ import { FC } from "react"
 import { useState } from "react"
 import { Input } from "../../ui/Input/Input"
 
-
-interface ILesson {
+const url = "http://127.0.0.1:5000"
+interface ISubject {
     num: number
-    grades: string[]
-    setGrades: (values: string[]) => void
     subjects: string[]
     setSubjects: (values: string[]) => void
 }
 
-const Lesson: FC<ILesson> = ({num, grades, setGrades, subjects, setSubjects}) => {
+interface IGrade {
+    num: number
+    grades: string[]
+    setGrades: (values: string[]) => void
+}
+
+const Subject : FC<ISubject> = ({num, subjects, setSubjects}) => {
     return(
     <div>
-        <Input title={"Класс"} value={grades[num]} setValue={value => {
-            const copy = [...grades]
-            copy[num] = value
-            setGrades(copy)
-        }}/>
         <Input title={"Предмет"} value={subjects[num]} setValue={value => {
             const copy = [...subjects]
             copy[num] = value
@@ -28,38 +27,52 @@ const Lesson: FC<ILesson> = ({num, grades, setGrades, subjects, setSubjects}) =>
     )
 }
 
-const Classrom = () => {
-    
-}
-
-const Time = () => {
-
-}
-
-export const CreateTimeTable = () => {
-    const [grades, setGrades] = useState<string[]>([])
-    const [subjects, setSubjects] = useState<string[]>([])
-    
-
-    return (
-        <div className="create-page">
-            {grades.map((f=grade, i) => <Lesson 
-            num={i} 
-            grades={grades} 
-            setGrades={setGrades}
-            subjects={subjects}
-            setSubjects={setSubjects}
-            />)}
-
-            <button onClick={() => {
-                setGrades([...grades, ""])
-                setSubjects([...subjects, ""])
-            }}>+</button>
-
-            <button onClick={() => {
-                setGrades.pop()
-                setSubjects.pop()
-            }}>-</button>
-        </div>
+const Grade : FC<IGrade> = ({num, grades, setGrades}) => {
+    return(
+    <div>
+        <Input title={"Класс"} value={grades[num]} setValue={value => {
+            const copy = [...grades]
+            copy[num] = value
+            setGrades(copy)
+        }}/>
+    </div>
     )
 }
+    <div className="create-page">
+        {grades.map(i) => <Grade
+        num={i} 
+        grades={grades} 
+        setGrades={setGrades}
+        />)}
+
+        <button onClick={() => {
+           setGrades([...grades, ""])
+         }}>+</button>
+
+    <div className="create-page">
+        {subjects.map(i) => <Subject
+        num={i} 
+        subjects={subjects} 
+        setSubjects={setSubjects}
+        />)}
+
+        <button onClick={() => {
+            setSubjects([...subjects, ""])
+        }}>+</button>
+
+        <button onClick={() => {
+            setGrades.slice(0, -1)
+            setSubjects.slice(0, -1)
+        }}>-</button>
+
+        <button onClick={() => {
+                alert(`${grades}: ${subjects}`)
+                fetch(url, {
+                    headers: {
+                      'Content-Type': 'application/json'
+                  },
+                    method: 'POST',
+                    body: grades
+                  })
+            }}>Отправить</button>
+    </div>
